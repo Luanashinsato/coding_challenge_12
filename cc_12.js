3ae293046beb73690589be9b7dc44daadc24d249
Luanashinsato <lshinsato@usf.edu>
2025-03-02 19:10:07 -0500

    Task 2 - Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -15,3 +15,18 @@ revenueCard.innerHTML = "<h3>Revenue</h3> <p>$0</p>";
 
 // Appending the new metric card to the dashboard container
 dashboard.appendChild(revenueCard);
+
+
+
+// TASK 2: Updating Dashboard Metrics – Working with NodeLists and Arrays
+// Selecting all elements with the class "metric-card"
+const metricCards = document.querySelectorAll(".metric-card");
+
+// Converting the resulting NodeList into an array
+const metricCardsArray = Array.from(metricCards); 
+
+// Using the array method .forEach() to update each card’s inner text 
+metricCardsArray.forEach(card => {
+    const header = card.querySelector("h3"); 
+    header.textContent += " - Updated" // Appending " - Updated" to the headers
+})
